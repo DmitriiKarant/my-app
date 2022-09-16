8142334b0f54b1fdf472ddb87ba87a9623b40742
Dmitrii Karant <Dmitrii_Karant@mail.ru>
2022-09-16 17:10:11 +0300

    add task 1 module 2/3

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './app';
+
+const container = document.getElementById('root');
+const Root = createRoot(container);
+Root.render(<App />);
+
